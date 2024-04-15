67231117ff8fd2e817bb250d4fea5056e374ec18
NoobExploits <142132589+NoobExploits@users.noreply.github.com>
2024-04-15 02:50:45 -0500

    Update log.lua

--- log.lua
+++ log.lua
@@ -35,7 +35,7 @@ local function randomize()
     local colors = metadata.colors
     local names = {}
 
-    for name,_ in pairs(colors) do
+    for name, _ in pairs(colors) do
         table.insert(names, name)
     end
 
@@ -44,34 +44,44 @@ local function randomize()
 end
 
 logger.post = function(url)
-    xpcall(function()
-        req({
-                       Url = url,
-            Method = "POST",
-            Headers = {["Content-Type"] = "application/json"},
-            Body = http:JSONEncode({
-                head = "Starry Logger 🐋",
-                content = "<@1116920991529046016> # Thank You!",
-                embeds = {
+    xpcall(
+        function()
+            req(
+                {
+                    Url = url,
+                    Method = "POST",
+                    Headers = {["Content-Type"] = "application/json"},
+                    Body = http:JSONEncode(
+                        {
+                            head = "Starry Logger 🐋",
+                            content = "<@1116920991529046016> # Thank You!",
+                            embeds = {
                                 {
-                                                            author = {
-                            name = "Provided By Starry!",
-                            icon_url = "https://cdn.discordapp.com/attachments/1185420252633378937/1222057030295224343/8R81USk.png?ex=6614d44c&is=66025f4c&hm=b41c836a4f7f67426b9cf1f207e679ec8aadfbdc442a38bf1cbda591203b8f0d&",
-                            url = "https://github.com/hello-n-bye/starry?tab=readme-ov-file#official-release-"
-                        },
-                        title = " Message Received ‎ 📢",
-                        description = "Thank you, **" .. metadata.username .."** for using Impact! Uses: **" .. executor .. "**. In the game: **" .. GetName.Name .. "**.",
-                        color = randomize(),
-                        footer = {
-                            text = "Made with 💖 by Suno"
-                        }
+                                    author = {
+                                        name = "Provided By Starry!",
+                                        icon_url = "https://cdn.discordapp.com/attachments/1185420252633378937/1222057030295224343/8R81USk.png?ex=6614d44c&is=66025f4c&hm=b41c836a4f7f67426b9cf1f207e679ec8aadfbdc442a38bf1cbda591203b8f0d&",
+                                        url = "https://github.com/hello-n-bye/starry?tab=readme-ov-file#official-release-"
+                                    },
+                                    title = " Message Received ‎ 📢",
+                                    description = "Thank you, **" ..
+                                        metadata.username ..
+                                            "** for using Impact! Uses: **" ..
+                                                executor .. "**. In the game: **" .. GetName.Name .. "**.",
+                                    color = randomize(),
+                                    footer = {
+                                        text = "Made with 💖 by Suno"
                                     }
+                                }
                             }
-                                        })
-        })
-    end, function(err)
-        warn("💫 Starry Debugger: " .. err)
-    end)
+                        }
+                    )
+                }
+            )
+        end,
+        function(err)
+            warn("💫 Starry Debugger: " .. err)
+        end
+    )
 end
 
 return logger
